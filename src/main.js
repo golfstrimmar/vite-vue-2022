8f0ea38e1b13bbfe26a058e294decbdbec8f8d7b
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2022-09-11 01:18:04 +0200

    11.09

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import { Quasar } from "quasar";
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+import "./../node_modules/quasar/dist/quasar.css";
+
+
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
