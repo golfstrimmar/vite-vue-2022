0088b9f35476300c9c3961152a763304ca38937f
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2022-10-24 15:31:17 +0200

    24,10

--- src/viewes/Home.vue
+++ src/viewes/Home.vue
@@ -2,7 +2,50 @@
 </script>
 
 <template>
-  <h3 class="text-h3  text-uppercase">Heimat</h3>
+  <h3 class=" text-h3  text-uppercase">Einleitung</h3>
+  <p> 
+  &emsp;Diese Anwendung wurde als Sammlung von Referenzmaterialien mit Beispielen für ein russischsprachiges Publikum
+  konzipiert.</p>
+  <p>&emsp;Als sie jedoch beim Erlernen der deutschen Sprache Fortschritte machten, stellte sich heraus, dass es für Anfänger
+  einfacher war, nach Gehör zu verstehen und zu lesen, als deutsche Sätze zu konstruieren. Daher wurde die Anwendung um
+  kleine Simulatoren zum Üben der Bildung deutscher Redewendungen ergänzt.</p>
+  <p>&emsp;Jeder Schieberegler auf der Wörterbuchseite ist eine Reihe von Phrasen, die durch ein gemeinsames Thema vereint sind.
+  Durch Klicken auf die Schaltfläche können Sie die Übersetzung des Satzes ins Russische sehen.</p>
+  <p>&emsp;Die Autorisierungsseite wird nur vom Administrator benötigt, um Ergänzungen an der Anwendung vorzunehmen.</p>
+  <p>&emsp;Im Allgemeinen hilft die Anwendung, Automatismus bei der Verwendung von Präpositionen, Artikeln, Pronomen und einigen
+  häufig verwendeten deutschen Redewendungen zu erreichen.</p>
+  <p>&emsp;Aus Sicht der Webentwicklung basiert diese Anwendung auf dem vue-3-Framework Quasar Framework und verfügt über eine
+  Autorisierungslogik, die mit Firebase interagiert.</p>
+  <br>
+  <p class="low">
+    Viel Spaß beim Deutsch lernen mit der deutsch-d26e4 App!</p>
+    <h3 class=" text-h3  text-uppercase">введение</h3>
+    <p>&emsp;Это приложение было задумано как сборник справочных материалов с примерами для русскоязычной аудитории.</p>
+    <p>&emsp;Однако , по мере продвижения в изучении немецкого языка , оказалось , что новичкам легче понимать на слух и читать, чем
+    конструировать немецкие фразы. Так приложение было дополнено небольшими тренажерами для отработки навыков создания
+    немецких фраз.</p>
+    <p>&emsp;Каждый слайдер на странице словаря представляет собой набор фраз , объединенных общей темой. Кликнув на кнопку, можно
+    посмотреть перевод фразы на русский язык.</p>
+    <p>&emsp;Страница авторизации нужна только администратору для внесения дополнений в приложение.</p>
+    <p>&emsp;В целом приложение помогает добиться автоматизма при употреблении предлогов, артиклей , местоимений и некоторых часто используемых немецких фраз.</p>
+    <p>&emsp;С точки зрения веб разработки данное приложение собрано на фреймверке vue-3 , Quasar Framework и имеет логику
+    авторизации и взаимодействия с firebase.</p>
+    <p class="low">&emsp;Получите удовольствие в изучении немецкого языка с приложением deutsch-d26e4 !</p>
+  
+
+
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h3{
+  margin: 30px 0 ;
+}
+p{
+  font-size: 20px;
+  font-weight: 700;
+  margin: 0 0 0 0;
+  &.low{
+text-align: center;
+  }
+}
+</style>
