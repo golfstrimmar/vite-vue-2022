c2fd56ba99f51af2e915b01bfd300d2aa46dc2d0
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2023-10-07 15:14:17 +0200

    uuu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,18 +3,38 @@ header
   SvgIcon(name='vite' )
   SvgIcon(name='vue' )
 
-  h2 component Header
+  router-link(v-for="link in links" :key="link.name" :to="link.href")
+    | {{ link.name }}
 
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import SvgIcon from '@/components/SvgIcon.vue'
+const links = ref([
+  { name: "Home", href: "/" },
+  // { name: "KitTest", href: "/kit-test" },
+]);
 </script>
 
 <style lang="scss" scoped>
 header {
   @include flex-aligne-center;
+  padding: 20px 10px;
   background: rgb(143, 189, 250);
+
+  svg {
+    width: 30px;
+    height: 30px;
+    margin: 0 10px 0 0;
+  }
+
+  .router-link {
+    color: $greyish-brown;
+  }
+
+  // .router-link-active {
+  //   color: red;
+  // }
 }
 </style>
