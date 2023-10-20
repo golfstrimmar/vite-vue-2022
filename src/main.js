c0109efae6f755475e5b9566cadaab0f6bd49ec5
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2023-10-20 17:14:01 +0200

    20

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import { createPinia } from "pinia";
 
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyAmo_0Q4sIuRQkBNp3F5_bStKK8LoHVwes",
-  authDomain: "dtraining.firebaseapp.com",
-  projectId: "dtraining",
-  storageBucket: "dtraining.appspot.com",
-  messagingSenderId: "499479318579",
-  appId: "1:499479318579:web:e66990da02456fffcad346",
+  apiKey: "AIzaSyCTr_tR1MbYjHurQgAfdgEntqZQ150rZyA",
+  authDomain: "deutsch-d26e4.firebaseapp.com",
+  databaseURL:
+    "https://deutsch-d26e4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "deutsch-d26e4",
+  storageBucket: "deutsch-d26e4.appspot.com",
+  messagingSenderId: "1002823720200",
+  appId: "1:1002823720200:web:c70fc15791d9ed502520bd",
 };
 
 initializeApp(firebaseConfig);
