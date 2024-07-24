e56f1d6688e101f8795bed5e2c4ad29419dc3e1f
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2024-07-25 01:45:39 +0200

    jjj

--- src/components/Copy.vue
+++ src/components/Copy.vue
@@ -1,13 +1,10 @@
 <template lang='pug'>
-.item
-	.copy(@click='handlerCopy()' ref = 'copy' :class="[(activeItem == true) ? '_is-active' : '']"
-) {{props.text}}
-
+.tags__item.item
+	.copy(@click='handlerCopy()' ref = 'copy' :class="[(activeItem == true) ? '_is-active' : '']") {{text}}
 </template>
 <script setup>
-import { ref } from 'vue'
-// В потомке
-// button(type = "button" @click = "$emit('someEvent',text) , Button($event)").btn.but - wave { { text } }
+import { ref, defineEmits } from 'vue';
+
 const props = defineProps({
 	text: {
 		type: String,
@@ -15,29 +12,37 @@ const props = defineProps({
 	}
 })
 
-let singles = ref([])
-const copy = ref(null)
-const activeItem = ref(false)
 
+let singles = ref([]);
+const copy = ref(null);
+const activeItem = ref(false);
+const emit = defineEmits(['someEvent'])
+const handlerOffset1 = () => {
+	offset.value = 1;
+}
 const handlerCopy = () => {
 	activeItem.value = !activeItem.value;
 	let add = document.createElement('textarea')
+	add.style.cssText = "position: absolute;z-index: -2;max-height: 0; border: none;"
 	document.querySelector('body').appendChild(add)
+
 	add.innerHTML = copy.value.innerHTML
 	add.select();
 	document.execCommand('copy');
-
-	setTimeout(function () { add.remove(); activeItem.value = !activeItem.value; }, 2000)
+	emit('someEvent', add.innerHTML)
+	setTimeout(function () { add.remove(); activeItem.value = !activeItem.value; }, 300)
 }
-
 
 </script>
 <style lang='scss' scoped>
 .copy {
 	cursor: pointer;
+	background-color: $lime-1;
+	padding: 2px;
 
 	&:hover {
-		color: $red-5;
+		color: $lime-2;
+		background-color: $deep-purple-3;
 		@include transition;
 	}
 
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,68 @@
 <template lang="pug">
-.hero 
-  .container
-    .tags__body
-      .tags__item
-        Copy(:text='div')
+.container
+  .hero 
+    button(type="button" @click='handelClean').hero__reset reset
+    .hero__plaza
+      .tempContent(v-for="item in Result" :key="item.id" @click = "delitTempItem(item.id)"  ref='some') {{ item.data }}
+    .hero__body
+      Copy(:text="item.dataText"  @someEvent = "someEvent" v-for="item in copyData" :key="item.i")
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import Copy from '@/components/Copy.vue'
+import { ref, reactive, computed } from 'vue'
+import Copy from './Copy.vue';
+var Result = reactive([{ id: 0, data: '' }]);
+var activePlaza = ref('');
+const Plaza = ref(null);
+const temp = ref(null)
+const someEvent = (data) => {
+  Result.push({ id: Result.length, data: data });
+}
+const delitTempItem = (id) => {
+  Result.forEach((cell) => {
+    if (cell.id == id) {
+      var indexCell = Result.indexOf(cell);
+      Result.splice(indexCell, 1);
+    }
+  });
+}
+
+const handelClean = () => {
+  Result.splice(0, Result.length);
+}
+const copyData = [
+  { i: 1, dataText: 'section(id="hero").hero.rel' },
+  { i: 2, dataText: '.best__' },
+  { i: 3, dataText: 'p' },
+  { i: 4, dataText: 'span' },
+  { i: 5, dataText: 'header' },
+  { i: 6, dataText: 'nav' },
+  { i: 7, dataText: 'ul' },
+  { i: 8, dataText: 'li' },
+  { i: 9, dataText: 'svg use(xlink: href="#!" rel=" ") ' },
+  { i: 10, dataText: 'a(href="#!")' },
+  { i: 11, dataText: 'a.btn.btn-success.but-wave.popups-init-js(rel="#popup1" href="#!")  p' },
+  { i: 12, dataText: '.container' },
+  { i: 13, dataText: 'h2' },
+  { i: 14, dataText: 'h3' },
+  { i: 15, dataText: 'body' },
+  { i: 16, dataText: 'title' },
+  { i: 17, dataText: 'text' },
+  { i: 18, dataText: 'blocks' },
+  { i: 19, dataText: 'items' },
+  { i: 20, dataText: 'item' },
+]
 
 
+// const copyPlaza = () => {
+//   activePlaza.value = !activePlaza.value;
+//   let add = document.createElement('textarea')
+//   document.querySelector('body').appendChild(add)
+//   add.innerHTML = Plaza.value.innerHTML
+//   add.select();
+//   document.execCommand('copy');
+//   setTimeout(function () { add.remove(); activePlaza.value = !activePlaza.value; }, 300)
+// }
 
 
 // const myCopy = () => {
@@ -52,5 +104,36 @@ import Copy from '@/components/Copy.vue'
 </script>
 
 <style lang="scss">
-.hero {}
+.hero {
+  display: grid;
+  grid-template-columns: 400px 1fr;
+  column-gap: 5px;
+  row-gap: 50px;
+
+  &__reset {
+    @include transition;
+    position: absolute;
+    transform: translate(0px, -31px);
+    padding: 5px 182px;
+
+    &:hover {
+      background-color: $pink-6;
+      color: white;
+    }
+  }
+
+  .tempContent {
+    &:hover {
+      cursor: pointer;
+      outline: 2px solid $yellow-10;
+      background-color: $pink-6;
+    }
+  }
+
+  &__plaza {
+    border: 2px solid $indigo-13;
+  }
+
+  &__body {}
+}
 </style>
