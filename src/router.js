80909d92f6b1738f8a24300aa24f61ec86e95795
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2024-08-05 12:17:20 +0200

    hhh

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "@/views/index.vue";
+import Something from "@/views/Something.vue";
+
+const routes = [
+  { path: "/", name: "Index", component: Index },
+  { path: "/something", name: "Something", component: Something },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: "/", name: "Index", component: Index },
-    
-  ],
+  routes,
 });
 
 export default router;
---
+++ src/views/Something.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+h2 Something
+</template>
+
+
+<script setup>
+</script>
+
+<style lang="scss"></style>
