286b42a98f7be55e35a7ffd37f83571f1cb63fb0
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2024-08-21 08:00:38 +0200

    gggg

---
+++ src/components/AuthInput.vue
@@ -0,0 +1,9 @@
+<template lang='pug'>
+input(v-model="password" type="password" placeholder="Password")
+
+</template>
+<script setup>
+import { ref } from 'vue';
+
+</script>
+<style lang='scss' scoped></style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,28 +3,66 @@ div
 	h1 Register
 	input(v-model="email" placeholder="Email")
 	input(v-model="password" type="password" placeholder="Password")
+
 	button(@click="register") Register
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router' // import router
+const router = useRouter();
 const email = ref('')
 const password = ref('')
 
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-	.then((userCredential) => {
-		// Signed up 
-		const user = userCredential.user;
-		console.log(user);
-	})
-	.catch((error) => {
-		const errorCode = error.code;
-		const errorMessage = error.message;
-		console.log(errorMessage);
-	});
+
+const register = async () => {
+	try {
+		await createUserWithEmailAndPassword(auth, email.value, password.value);
+		alert("Sucsess");
+	} catch (error) {
+		switch (error.code) {
+			case "auth/email-allready-in-use":
+				alert("E-Mail bereits verwendet");
+				break;
+
+			case "auth/invalid-email":
+				alert("Das Format der E-Mail-Adresse ist ungültig.");
+				break;
+
+			case "auth/operation-not-allowed":
+				alert("Operation ist verboten");
+				break;
+
+			case "auth/weak-password":
+				alert("Das Passwort muss mindestens 6 Zeichen lang sein");
+				break;
+
+			default:
+				alert(error.code);
+		}
+		return;
+	}
+
+	router.push("/");
+}
+
+
+
+
+// createUserWithEmailAndPassword(auth, email, password)
+// 	.then((userCredential) => {
+// 		// Signed up 
+// 		const user = userCredential.user;
+// 		alert(user)
+// 	})
+// 	.catch((error) => {
+// 		const errorCode = error.code;
+// 		const errorMessage = error.message;
+// 		alert(errorMessage)
+// 	});
+
 </script>
 <style lang='scss' scoped></style>
