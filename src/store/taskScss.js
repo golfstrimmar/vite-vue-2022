ebfb56e7a19e1adfe2d5bfcc70421020e5f75488
golfstrimmar <yushinbox@gmail.com>
2024-12-15 07:20:06 +0100

    hhh

--- src/store/taskScss.js
+++ src/store/taskScss.js
@@ -67,6 +67,10 @@ place-items: center;`,
       { i: 10, dataText: "background-position: 50%;" },
       { i: 10, dataText: "backdrop-filter: blur(15px);" },
       { i: 10, dataText: "mix-blend-mode: difference;" },
+      {
+        dataText: `columns: 300px;
+	column-gap: var(--gap);`,
+      },
     ],
 
     [
@@ -106,9 +110,13 @@ place-items: center;`,
       { i: 2, dataText: "gap: 37px 36px;" },
       { i: 3, dataText: "grid-auto-flow: row;" },
       {
-        dataText: "равные колонки по размеру контента\n" +"grid-auto-flow: column;",
-      }, {
-        dataText: "выстраивает в одну колонку  по размеру контента\n" +"display: grid;\n" +
+        dataText:
+          "равные колонки по размеру контента\n" + "grid-auto-flow: column;",
+      },
+      {
+        dataText:
+          "выстраивает в одну колонку  по размеру контента\n" +
+          "display: grid;\n" +
           "    grid-template-columns: min-content;\n" +
           "    grid-auto-rows: min-content;",
         description: "выстраивает в одну колонку  по размеру контента ",
@@ -116,8 +124,7 @@ place-items: center;`,
 
       {
         i: 6,
-        dataText: "прижимает к верху\n" +
-          "grid-auto-rows: max-content;",
+        dataText: "прижимает к верху\n" + "grid-auto-rows: max-content;",
         description: "прижимает к верху",
       },
       { i: 7, dataText: 'grid-template-areas: "a b c e" "a d c e";' },
